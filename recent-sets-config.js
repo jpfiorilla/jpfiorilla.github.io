93e410d9125059c9e47c865ea150e48d008a5406
John <jpfiorilla@gmail.com>
2025-08-04 21:27:22 -0400

    dont poll maybe

--- recent-sets-config.js
+++ recent-sets-config.js
@@ -1,3 +1,4 @@
 window.STARTGG_TOKEN = "a0249a0ca221443efcdd5e9f9a5aa1ae";
 window.STARTGG_URL =
   "https://www.start.gg/tournament/nycmelee-s-stock-exchange-13/event/melee-singles";
+window.ENABLE_POLLING = false;
