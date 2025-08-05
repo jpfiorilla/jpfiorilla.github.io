3e4018953ac3f48d8dde3504bba1ef799364717e
John <jpfiorilla@gmail.com>
2025-08-05 15:50:49 -0400

    h2h toggle init

--- melee/recent-sets-config.js
+++ melee/recent-sets-config.js
@@ -2,3 +2,4 @@ window.STARTGG_TOKEN = "a0249a0ca221443efcdd5e9f9a5aa1ae";
 window.STARTGG_URL =
   "https://www.start.gg/tournament/nycmelee-s-stock-exchange-13/event/melee-singles";
 window.ENABLE_POLLING = false;
+window.ENABLE_DEV_HEAD_TO_HEAD = true;
