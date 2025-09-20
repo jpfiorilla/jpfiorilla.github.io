42c7863d08a4311c2026eb7384dc8c4de127762f
John <jpfiorilla@gmail.com>
2025-09-20 10:44:45 -0400

    gitignore

--- public/melee/recent-sets-config.js
+++
@@ -1,5 +0,0 @@
-window.STARTGG_TOKEN = "a0249a0ca221443efcdd5e9f9a5aa1ae";
-window.STARTGG_URL =
-  "https://www.start.gg/tournament/nycmelee-s-stock-exchange-15-collision-prelocal/event/melee-singles";
-window.ENABLE_POLLING = true;
-window.ENABLE_DEV_HEAD_TO_HEAD = true;
